371c2be138e056ff56453387eab94b04c197207e
Quercy-tech <alexghezha@gmail.com>
2024-08-16 17:28:06 +0300

    New menu on phones, sevral fixes

--- js/app.js
+++ js/app.js
@@ -8,3 +8,20 @@ if (isMobile()) {
 } else {
   document.body.classList.add('desktop');
 }
+
+function menuClick() {
+  let menuContent = document.getElementById('menuContent')
+  let menuIcon = document.getElementById('menuIcon')
+  if (menuContent.style.display === 'block') {
+    menuContent.style.display = 'none';
+    menuIcon.src = 'HamburgerWhite.png'
+  } else {
+    menuContent.style.display = 'block';
+    menuIcon.src = 'Close.png'
+  }
+}
+
+
+
+
+
